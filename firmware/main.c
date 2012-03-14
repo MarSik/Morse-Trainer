a8126fe692572b621d7684002ac5d736377886b4
Martin Sivak <msivak@redhat.com>
2012-03-14 18:50:19 +0100

    update main to read the samplerate from flash and to play whole strings

--- firmware/main.c
+++ firmware/main.c
@@ -23,61 +23,121 @@ void setup(void)
     flash_init();
 }
 
-void play_character(uint8_t id)
+
+void play_characters(uint8_t *chs)
 {
     uint8_t a2,a1,a0;
-    uint16_t l;
+    uint16_t l = 0;
 
-    if (id == ' '){
-        _delay_ms(500);
-        return;
-    }
-
-    // init audio
-    audio_wav_init(8000);
-    
-    // get sound data for morse char
+    // init audio, char 0 has info about bitrate
     flash_begin();
-    l = flash_info(id, &a2, &a1, &a0);
+    l = flash_info(0, &a2, &a1, &a0);
     flash_end();
     
-    flash_begin();
-    flash_read_init(a2, a1, a0);
+    audio_wav_init(l);
     
-    // prefill buffer
-    while (!audio_buffer_full(1) && l>0) {
-        audio_wav_data(flash_read());
-        l--;
+    while (*chs) {
+        // get sound data for char
+        flash_begin();
+        l = flash_info(*chs, &a2, &a1, &a0);
+        flash_end();
+    
+        flash_begin();
+        flash_read_init(a2, a1, a0);
+    
+        // buffer is full, read is prepared, let it play
+        if (audio_buffer_full(1)) break;
+
+        // prefill buffer
+        while (!audio_buffer_full(1) && l>0) {
+            audio_wav_data(flash_read());
+            l--;
+        }
+
+        if (l==0) { // whole character was read already, set up next character
+            flash_end();
+            chs++;
+        }
+        else break; // buffer is full, but the current character has data left, let it play
     }
     
     // start playing
     audio_play();
     
-    // refill buffer when needed
-    while (l>0) {
-        while (audio_buffer_full(1));
-        audio_wav_data(flash_read());
-        l--;
+    // refill buffer when needed (read has already been prepared)
+    // if there are data for current char, read them
+    // if no, move to the next char
+    while(*chs) {
+        while (l>0) {
+            while (audio_buffer_full(1));
+            audio_wav_data(flash_read());
+            l--;
+        }
+
+        // move to the next character
+        chs++;
+        if (*chs) {
+            flash_end(); // end reading current char
+
+            // get info about new char
+            flash_begin();
+            l = flash_info(*chs, &a2, &a1, &a0);
+            flash_end();
+            
+            // inititalize read
+            flash_begin();
+            flash_read_init(a2, a1, a0);
+        }
+        else flash_end(); // no additional character
     }
-    flash_end();
     
     // wait till everything is played
     while(!audio_buffer_empty() || !audio_buffer_finished());
     audio_stop();
 }
 
-uint8_t play_morse(uint8_t c)
+void play_character(uint8_t id)
 {
-    uint8_t v_bitmask = MORSE_MASK(c);
-    uint8_t v_length = MORSE_LEN(c);
-    uint8_t v_id = MORSE_ID(c);
-    
+    uint8_t s[] = {id, 0x0};
+    play_characters(s);
+}
+
+uint8_t play_morse(uint8_t *chs)
+{
+    uint8_t v_id = 0x0; // last played char
+
     // init morse
     audio_morse_init(600, 6);
-    
-    // add morse data to buffer and play it 
-    audio_morse_data(v_length, v_bitmask, 7);
+
+    // prefill buffer
+    while (*chs && !audio_buffer_full(2)) {
+        uint8_t v_bitmask = MORSE_MASK(*chs);
+        uint8_t v_length = MORSE_LEN(*chs);
+        v_id = MORSE_ID(*chs);
+
+        // add morse data to buffer and play it 
+        audio_morse_data(v_length, v_bitmask, 7);
+
+        chs++;
+    }
+
     audio_play();
+
+    // keep buffer filled
+    while (*chs) {
+        uint8_t c = *chs;
+        uint8_t v_bitmask = MORSE_MASK(c);
+        uint8_t v_length = MORSE_LEN(c);
+        v_id = MORSE_ID(c);
+
+        while(audio_buffer_full(2)); // wait till there is some space in the buffer
+
+        // add morse data to buffer and play it 
+        audio_morse_data(v_length, v_bitmask, 7);
+
+        chs++;
+    }
+
     while(!audio_buffer_empty() || !audio_buffer_finished());
     audio_stop();
 
@@ -95,17 +155,19 @@ int main(void)
 
     _delay_ms(2000);
 
-    char welcome[] = "vitejte v morse testeru.";
-    while(welcome[c]) play_character(welcome[c++]);
-    c = 0;
+    uint8_t welcome[] = "vitejte v morse testeru.";
+    play_characters(welcome);
 
     _delay_ms(1500);
 
     while(1) {
-        uint8_t v_id = play_morse(c);
+        uint8_t s[] = {c, 0x0};
+        uint8_t v_id = play_morse(s);
         c = (c + 1) % MORSE_TABLE_LEN;
-        play_character(v_id);
-        _delay_ms(4000);
+        if (v_id) {
+            play_character(v_id);
+            _delay_ms(4000);
+        }
     }
 
     return 0;
